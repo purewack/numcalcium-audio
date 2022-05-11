08ecd3f2e59f18046963a3299a12463d2677e97c
purewack <bobimaster15@gmail.com>
2022-05-11 10:34:22 +0100

    remove hard coded libintdsp

--- libintdsp/adr_t.h
+++
@@ -1,8 +0,0 @@
-#include "libintdsp.h"
-
-struct adr_t{
-  spl_t spl;
-  int32_t a_v, r_v, a;
-  int32_t acc,vv,v;
-  bool state, old_state;
-};
--- libintdsp/lpf_t.h
+++
@@ -1,9 +0,0 @@
-#include "libintdsp.h"
-
-struct lpf_t{
-  spl_t spl, 
-  int16_t h, h2, h3;
-  int16_t a;
-};
-
-void proc_lpf(lpf_t* node);
