9d67250163abf154d8bd44defdaed3d4b688ff7b
purewack <bobimaster15@gmail.com>
2022-05-18 23:49:21 +0100

    trying to fix encoder

--- stm32pwmaudip.ino
+++ stm32pwmaudip.ino
@@ -80,42 +80,102 @@ int16_t spl_out_a,spl_out_b;
 
 struct t_io {
   volatile uint8_t state;
-  volatile uint8_t old_state;
+  volatile uint8_t state_old;
 };
 
 struct HW
 {
-  volatile t_io io[20];
-  const uint8_t rows[6] = {ROW_A, ROW_B, ROW_C, ROW_D, ROW_E, ROW_F};
-  const uint8_t cols[4] = {SEG_A, SEG_B, SEG_C, SEG_D};
-  uint8_t itr;
+  uint8_t ok;
+  int turns;
+  int turns_old;
+  int turns_state;
+  int turns_state_old;
+  volatile t_io io[24];
+  const uint8_t seq_row[7] = {14,15,3,4,8};
+  uint8_t itr;  
   uint8_t row;
+  uint8_t op;
   timer_dev* timer;
 } kmux;
 
-void kmux_irq(){
-  digitalWrite(kmux.rows[kmux.row],1);
-  kmux.io[kmux.itr+0].state = digitalRead(kmux.cols[0]);
-  kmux.io[kmux.itr+1].state = digitalRead(kmux.cols[1]);
-  kmux.io[kmux.itr+2].state = digitalRead(kmux.cols[2]);
-  kmux.io[kmux.itr+3].state = digitalRead(kmux.cols[3]);
-  digitalWrite(kmux.rows[kmux.row],0);
+void kmux_init(){
+  gpio_set_mode(GPIOA, 14, GPIO_OUTPUT_PP);
+  gpio_set_mode(GPIOA, 15, GPIO_OUTPUT_PP);
+  gpio_set_mode(GPIOB, 3, GPIO_OUTPUT_PP);
+  gpio_set_mode(GPIOB, 4, GPIO_OUTPUT_PP);
+  gpio_set_mode(GPIOB, 8, GPIO_OUTPUT_PP);
+  gpio_set_mode(GPIOB, 9, GPIO_OUTPUT_PP);
 
-  kmux.itr = (kmux.itr+4)%20;
-  kmux.row = (kmux.row+1)%5;
+  gpio_set_mode(GPIOB, 11, GPIO_INPUT_PD);
+  gpio_set_mode(GPIOB, 10, GPIO_INPUT_PD);
+  gpio_set_mode(GPIOB, 1, GPIO_INPUT_PD);
+  gpio_set_mode(GPIOB, 0, GPIO_INPUT_PD);
+
+  gpio_set_mode(GPIOA, 8, GPIO_INPUT_PD);
 }
 
+void kmux_irq(){
+
+  if(kmux.op == 0){
+    gpio_write_bit(kmux.row < 2 ? GPIOA : GPIOB, kmux.seq_row[kmux.row], 1);
+    kmux.op = 1;
+    return;
+  }
+
+  else if(kmux.op == 1){
+    auto a = GPIOB->regs->IDR;
+    kmux.io[kmux.itr+0].state = (a&(1<<11))>>11;
+    kmux.io[kmux.itr+1].state = (a&(1<<10))>>10;
+    kmux.io[kmux.itr+2].state = (a&(1<<1))>>1;
+    kmux.io[kmux.itr+3].state = (a&(1<<0))>>10;
+    gpio_write_bit(kmux.row < 2 ? GPIOA : GPIOB, kmux.seq_row[kmux.row], 0);
+    
+    kmux.itr = (kmux.itr+4)%20;
+    kmux.row = (kmux.row+1)%5;
+    kmux.op = 2;
+    return;
+  }
+  
+  else if(kmux.op == 2){
+    gpio_write_bit(GPIOB, 9, 1);
+    kmux.ok = gpio_read_bit(GPIOA, 8);
+    kmux.op = 3;
+    return;
+  }
+
+
+  kmux.op = 0;
+  auto a = GPIOB->regs->IDR;
+  kmux.io[20].state = (a&(1<<11))>>11;
+  kmux.io[21].state = (a&(1<<10))>>10;
+  gpio_write_bit(GPIOB, 9, 0);
+
+  kmux.turns_state = (kmux.io[20].state<<0) | (kmux.io[21].state<<1) | (kmux.io[20].state_old<<2) | (kmux.io[21].state_old<<3);
+  kmux.io[20].state_old = kmux.io[20].state;
+  kmux.io[21].state_old = kmux.io[21].state;
+
+  if(kmux.turns_state == 0b0001){
+    if(kmux.turns < 0) kmux.turns = 0;
+    kmux.turns++;
+  }
+  else if(kmux.turns_state == 0b1101){
+    if(kmux.turns > 0) kmux.turns = 0;
+    kmux.turns--;
+  }
+
+
+}
 
 void benchSetup(){
-  pinMode(PA0, OUTPUT);
+  gpio_set_mode(GPIOB, 14, GPIO_OUTPUT_PP);
 }
 
 void benchStart(){
-  GPIOA->regs->BSRR = 1<<0;
+  GPIOB->regs->BSRR = 1<<14;
 }
 
 void benchEnd(){
-  GPIOA->regs->BSRR = (1<<0)<<16;
+  GPIOB->regs->BSRR = (1<<14)<<16;
 }
 //TIM4 CH 1 = PB6
 //TIM4 CH 2 = PB7
@@ -229,18 +289,14 @@ void setup() {
 
 
 ///////////////////
-
-  for(int i=0; i<4; i++)
-    pinMode(kmux.cols[i], INPUT_PULLDOWN);
-  for(int i=0; i<6; i++)
-    pinMode(kmux.rows[i], OUTPUT);
+  kmux_init();
 
   kmux.itr = 0;
   kmux.timer = TIMER3;
   timer_pause(kmux.timer);
   //48M / 48 / 2000 = 25hz*20keys
   timer_set_prescaler(kmux.timer, 48);
-  timer_set_reload(kmux.timer, 2000);
+  timer_set_reload(kmux.timer, 1000);
   timer_attach_interrupt(kmux.timer, TIMER_UPDATE_INTERRUPT, kmux_irq);
   timer_enable_irq(kmux.timer, TIMER_UPDATE_INTERRUPT);
   timer_resume(kmux.timer);
@@ -283,6 +339,10 @@ void loop() {
   // LOGL(kmux.itr);
   // LOGL("-------");
   // delay(500);
+  if(kmux.turns != kmux.turns_old){
+    kmux.turns_old = kmux.turns;
+    Serial.println(kmux.turns);
+  }
 
   if(i2s.req){
     benchStart();
